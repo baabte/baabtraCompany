533e96576bcb718dda6d98bdb31e9243d6dfe9ac
jihin <jihin@baabte.com>
2015-06-22 12:16:41 +0530

    domain redirection by Jihin

--- config.js
+++ config.js
@@ -63,8 +63,8 @@ angular.module('baabtra')
   // }])
   .constant('bbConfig',{ //used for storing enviornment variables
 
-    // "BWS": "http://127.0.0.1:8000/",//local
-    "BWS": "http://services.baabtra.com/",//server - production
+     "BWS": "http://127.0.0.1:8000/",//local
+    //"BWS": "http://services.baabtra.com/",//server - production
     // "BWS": "http://server.mb-test.in/",//server-test
 
      "SARID":1, // SuperAdminRoleID
