70af90b0b46b312ad0b6547534c5b2b398727bbd
Lijin <lijin@baabte.com>
2015-06-26 15:01:00 +0530

    local changes

--- config.js
+++ config.js
@@ -64,8 +64,8 @@ angular.module('baabtra')
   .constant('bbConfig',{ //used for storing enviornment variables
 
     // "BWS": "http://127.0.0.1:8000/",//local
-    "BWS": "http://services.baabtra.com/",//server - production
-    // "BWS": "http://server.mb-test.in/",//server-test
+    // "BWS": "http://services.baabtra.com/",//server - production
+    "BWS": "http://server.baabtra.in/",//server-test
 
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
@@ -76,8 +76,8 @@ angular.module('baabtra')
 angular.module('angular-custom-form',[]) /*Config constant for angular custom form */
 .constant('bbConfig',{ //used for storing enviornment variables
      // "BWS": "http://127.0.0.1:8000/",//local
-     "BWS": "http://services.baabtra.com/",//server - production
-    // "BWS": "http://server.mb-test.in/",//server-test
+     // "BWS": "http://services.baabtra.com/",//server - production
+    "BWS": "http://server.baabtra.in/",//server-test
      "SARID":1, // SuperAdminRoleID
      "CURID":2, // CompanyUser
      "MURID":3, // MenteeUser
